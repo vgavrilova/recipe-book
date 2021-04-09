330aee224c90d0b0061b6a9cb027e33c44ce9882
Valeria Gavrilova <gavrivalery@gmail.com>
2021-04-09 13:37:58 +0200

    Outsource recipe data to the recipe item component :hammer:

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Recipe } from './../../recipes.model';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+  styleUrls: ['./recipe-item.component.css'],
 })
 export class RecipeItemComponent implements OnInit {
+  @Input() recipeItem: Recipe;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -18,6 +18,11 @@ export class RecipeListComponent implements OnInit {
       'A famous italian dessert',
       'https://static01.nyt.com/images/2017/04/05/dining/05COOKING-TIRAMISU1/05COOKING-TIRAMISU1-articleLarge.jpg'
     ),
+    new Recipe(
+      'Veggie Burger',
+      'No meat, no milk, no fear',
+      'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/1B8CA0A3-8D58-43D9-A2EB-1E834BA97F97/Derivates/EDCCBF93-5A3C-49E3-B4D6-CE86A55117FE.jpg'
+    ),
   ];
 
   constructor() {}
