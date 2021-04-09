3bdf5a38857ca245c4b34379967e9231f326a9f7
Valeria Gavrilova <gavrivalery@gmail.com>
2021-04-09 13:17:24 +0200

    Navigation with Event Binding and ngIf :sparkles:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'recipe-book';
+  toLoad: 'recipes' | 'shoppingList' = 'recipes';
+
+  onNavigate(componentToLoad) {
+    this.toLoad = componentToLoad;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
+  @Output() onLoadRecipes = new EventEmitter<'recipes' | 'shoppingList'>();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  loadRecipes() {
+    this.onLoadRecipes.emit('recipes');
+  }
+
+  loadShoppingList() {
+    this.onLoadRecipes.emit('shoppingList');
+  }
 }
